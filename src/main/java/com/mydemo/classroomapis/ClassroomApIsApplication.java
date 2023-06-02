6cf0866ebe1ead47b94a5f36a53afc236f028b11
asad-jf <asadzahid1479@gmail.com>
2023-06-02 10:13:00 +0500

    Create JF Test Environment

---
+++ src/main/java/com/mydemo/classroomapis/ClassroomApIsApplication.java
@@ -0,0 +1,13 @@
+package com.mydemo.classroomapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydemo/classroomapis/ClassroomApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydemo.classroomapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
