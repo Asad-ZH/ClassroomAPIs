01acad01f0b2cdb84fa8c6addd4504abed53e1c3
asad-jf <asadzahid1479@gmail.com>
2023-06-05 15:52:02 +0500

    Create Student APIs

--- src/main/java/com/mydemo/classroomapis/Service/StudentService.java
+++ src/main/java/com/mydemo/classroomapis/Service/StudentService.java
@@ -1,4 +1,42 @@
 package com.mydemo.classroomapis.Service;
 
+import com.mydemo.classroomapis.Entity.Subject;
+import com.mydemo.classroomapis.Entity.Student;
+import com.mydemo.classroomapis.Repository.StudentRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import com.mydemo.classroomapis.Entity.Teacher;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<String> getSubjects() {
+        Student student = fetchData();
+        return student.getSubjects().stream().map(Subject::getSubjectName).toList();
+    }
+
+    public Student fetchData() {
+
+        String username = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return studentRepository.findByUsername(username);
+    }
+
+    public List<String> getTeachers() {
+        Student student = fetchData();
+        return student.getTeachers().stream().map(Teacher::getTeacherName).toList();
+    }
 }
